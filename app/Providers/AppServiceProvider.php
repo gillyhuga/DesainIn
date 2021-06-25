0456808cf873a5b85cf9fe206a2e980debe01834
gillyhuga <gilly.huga@yahoo.co.id>
2021-06-26 00:09:50 +0700

    update service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        Schema::defaultStringLength(191);
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+//import this
+use Illuminate\Support\Facades\Schema;
+class AppServiceProvider extends ServiceProvider{
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register(){
+     //add this
+     Schema::defaultStringLength(191);
+  }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+   public function boot(){
+      //
+   }
 }
